eedca776ab016cb3942e265bb94f133a20fbd1e0
tan8888gh <tanveen_shakeel@hotmail.com>
2024-01-28 19:17:02 +0000

    First two rules of validation added

--- src/tanveenca1/TanveenCA1.java
+++ src/tanveenca1/TanveenCA1.java
@@ -18,17 +18,18 @@ public class TanveenCA1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         try {
             File file = new File("C:\\Users\\tanve\\Desktop\\students.txt");
             Scanner myReader = new Scanner(file);
 
+            /*Line Number will be used to check the current line in the students.txt file 
+              and to check if the line contains the valid data according to assignment rules*/
             int lineNumber = 1;
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
                 System.out.println("Currenly on Line number " + lineNumber + " Data: " + data);
                 String[] names = data.split(" ");
-
+                
                 if (lineNumber == 1) {
                     System.out.println("Student Name");
                     String firstName = names[0];
@@ -49,23 +50,10 @@ public class TanveenCA1 {
                     }
                     System.out.println("Last Name: " + lastName);
                 }
-
-                int numberOfClasses = 0;
-
-                if (lineNumber == 2) {
-                    if (data.charAt(0) > '1' && data.charAt(0) <= '8') {
-                        numberOfClasses = Character.getNumericValue(data.charAt(0));
-                    } else {
-                        throw new IllegalArgumentException("Number of classes is not valid");
-                    }
-                    System.out.println("Number of classes: " + numberOfClasses);
-                }
-
                 lineNumber++;
                 if (lineNumber > 3) {
                     lineNumber = 1;
                 }
-//                System.out.println(lastName);
             }
             myReader.close();
         } catch (IllegalArgumentException exc) {
@@ -73,23 +61,6 @@ public class TanveenCA1 {
         } catch (Exception exc) {
             exc.printStackTrace();
         }
-
-        try {
-            File file = new File("C:\\Users\\tanve\\Desktop\\status.txt");
-
-            if (file.createNewFile()) {
-                System.out.println("New file created");
-            } else {
-                System.out.println("already exists");
-            }
-            
-            FileWriter fileWriter = new FileWriter(file);
-            fileWriter.write("Testing");
-            fileWriter.close();
-        } catch (Exception exc) {
-            exc.printStackTrace();
-        }
-
     }
 
 }
