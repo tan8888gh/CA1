99f47481bd6b6f410c8e8c01ef9e1f3bdea35eeb
tan8888gh <tanveen_shakeel@hotmail.com>
2024-01-31 20:58:04 +0000

    more comments added for clarity

--- src/tanveenca1/TanveenCA1.java
+++ src/tanveenca1/TanveenCA1.java
@@ -40,6 +40,7 @@ public class TanveenCA1 {
 
                     System.out.println("Student Name");
                     String firstName = names[0];
+                    //this logic is validating input by using java built in methods
                     char[] firstNameArr = firstName.toCharArray();
                     for (int i = 0; i < firstNameArr.length; i++) {
                         if (!Character.isLetter(firstNameArr[i])) {
@@ -66,6 +67,7 @@ public class TanveenCA1 {
                 int numberOfClasses;
                 //Checks if the line number is 2 and if it is 2 it gets the current student object and set number of classes in it
                 if (lineNumber == 2) {
+                    //this logic is comparing ASCII code with characters from string
                     if (data.charAt(0) >= '1' && data.charAt(0) <= '8') {
                         numberOfClasses = Character.getNumericValue(data.charAt(0));
                     } else {
@@ -83,6 +85,7 @@ public class TanveenCA1 {
                     if (!Character.isDigit(data.charAt(0)) || !Character.isDigit(data.charAt(1))) {
                         throw new IllegalArgumentException("Invalid data Student id has first two characters other than numbers");
                     }
+                    //characters validation using java built in method Character.isLetter()
                     if (!Character.isLetter(data.charAt(2))
                             || !Character.isLetter(data.charAt(3))
                             || !Character.isLetter(data.charAt(4))) {
